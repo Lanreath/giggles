c97edf78662af3217c438470739dbd0179fc58b0
Lanreath <tyfei00@gmail.com>
2023-12-18 14:02:03 +0200

    Test env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log(process.env.VERCEL_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
