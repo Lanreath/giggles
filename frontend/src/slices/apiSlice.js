e7eee4c4881ca439727268592123dccd58b1d072
Lanreath <tyfei00@gmail.com>
2023-12-18 15:13:19 +0200

    Test

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://giggles-fdd233d1e684.herokuapp.com/', credentials: 'include' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://giggles-fdd233d1e684.herokuapp.com/', credentials: 'include', mode: 'cors' });
 
 export const apiSlice = createApi({
   baseQuery,
