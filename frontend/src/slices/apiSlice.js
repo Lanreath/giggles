f35698bc5c3940151b426093c1190f38dffe2f2b
Lanreath <tyfei00@gmail.com>
2023-12-18 14:33:01 +0200

    CHange env

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+import dotenv from 'dotenv';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: process.env.API_URL});
 
 export const apiSlice = createApi({
   baseQuery,
