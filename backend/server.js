c3c19a5472e0337414ed40f30889a3efbee91fcc
Lanreath <tyfei00@gmail.com>
2023-12-18 14:52:58 +0200

    Test

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
   {
-    origin: 'https://giggles-dusky.vercel.app/',
+    origin: 'https://giggles-dusky.vercel.app',
   }
 ));
 
