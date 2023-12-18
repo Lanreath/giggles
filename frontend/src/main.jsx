71f0abe5cf3ba1524d9c640e397b653d3f7677ef
Lanreath <tyfei00@gmail.com>
2023-12-18 14:35:08 +0200

    CHange env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import dotenv from 'dotenv';
+dotenv.config();
 import {
   createBrowserRouter,
   createRoutesFromElements,
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,5 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 import dotenv from 'dotenv';
+dotenv.config();
 
 const baseQuery = fetchBaseQuery({ baseUrl: process.env.API_URL});
 
